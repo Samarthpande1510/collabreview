50057db861faa6b45681f460ee40a02cbfc1ba06
Samarthpande1510 <samarthpande68@gmail.com>
2026-06-16 16:08:25 +0530

    Fixed up the models correctly, added refresh token

--- backend/models.py
+++ backend/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime, Float, ForeignKey, Text
-from datetime import datetime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text, Boolean
+from datetime import datetime,timedelta
 from database import Base
 import uuid
 
@@ -8,9 +8,11 @@ class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True, nullable=False)
-    password = Column(String, nullable=False)
+    password = Column(String, nullable=True)
     name = Column(String)
     created_at = Column(DateTime, default=datetime.utcnow)
+    google_id = Column(String, unique=True)
+    is_active = Column(Boolean, default=True)
 
 class Room(Base):
     __tablename__ = "rooms"
@@ -20,8 +22,10 @@ class Room(Base):
     owner_id = Column(Integer, ForeignKey("users.id"))
     language = Column(String)
     code_content = Column(Text)
+    updated_at = Column(DateTime,onupdate=datetime.utcnow)
     created_at = Column(DateTime, default=datetime.utcnow)
     share_mode = Column(String, default="reviewer")
+
 class Comment(Base):
     __tablename__ = "comments"
     id = Column(Integer, primary_key=True)
@@ -30,3 +34,13 @@ class Comment(Base):
     line_number = Column(Integer)
     content = Column(Text)
     created_at = Column(DateTime, default=datetime.utcnow)
+
+class RefreshToken(Base):
+    __tablename__ = "refreshtokens"
+    id = Column(Integer, primary_key = True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    token = Column(String)
+    expires_at = Column(DateTime,default=lambda: datetime.utcnow() + timedelta(days=7))
+    revoked = Column(Boolean,default=False)
+    created_at = Column(DateTime, default=datetime.utcnow)
+
