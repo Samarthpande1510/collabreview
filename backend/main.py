ee36241c3f2b38ae2884835a9fdd685426f17a09
Samarthpande1510 <samarthpande68@gmail.com>
2026-06-16 16:08:59 +0530

    Fixed up the main, removed useless junk

--- backend/main.py
+++ backend/main.py
@@ -16,8 +16,8 @@ app = FastAPI(
 
 
 origins = [
-    "http://localhost:3000",  # React default port
-    "http://localhost:5173",  # Vite default port
+    "http://localhost:3000", 
+    "http://localhost:5173", 
     "http://127.0.0.1:5173",
 ]
 
